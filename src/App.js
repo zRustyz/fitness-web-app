83e133f3b458498df8a634e698630c3e1fa38c67
Minh Le <mle21@uw.edu>
2023-02-24 19:01:15 -0800

    Add checkbox for body part

--- src/App.js
+++ src/App.js
@@ -1,49 +1,81 @@
-import React, { useState } from 'react'; //import React Component
-//import { RenderHeader } from './ExcercisePage';
-//import { RenderContent } from './ExcercisePage';
-import{Header} from './Header.js'
-import{Search} from './Search.js'
-import{TopRow} from './TopRow.js';
-import{Equipment}from './Equipment.js';
-import{BottomRow}from './BottomRow';
-import{Footer}from './Footer.js';
+import React, { useState } from 'react';
+import { Header } from './Header.js';
+import { Search } from './Search.js';
+import { TopRow } from './TopRow.js';
+import { Equipment } from './Equipment.js';
+import { BottomRow } from './BottomRow';
+import { Footer } from './Footer.js';
 import CardList from './components/CardList';
 import ExerciseSelectForm from './components/ExerciseSelectForm';
 
 function App(props) {
-  const [bodyPartFilter, setBodyPartFilter] = useState('');
+  const [equipmentFilter, setEquipmentFilter] = useState('');
+  const [includeArms, setIncludeArms] = useState(false);
+  const [includeLegs, setIncludeLegs] = useState(false);
+  const [includeChest, setIncludeChest] = useState(false);
+  const [includeShoulder, setIncludeShoulder] = useState(false);
+  const [includeBack, setIncludeBack] = useState(false);
 
-  //get sorted list of unique body parts. reduce array of objects into array of strings,
+  //get sorted list of unique equipment. reduce array of objects into array of strings,
   //convert to Set to get uniques, spread back into array, and sort
   const uniqueExercise = [...new Set(props.data.reduce((all, current) => {
-    return all.concat([current.body_part]);
+    return all.concat([current.equipment]);
   }, []))].sort();
 
-  // Filter the data prop based on the state filters
+  // Filter the data prop based on equipments and body part checkbox
   let displayedData = props.data;
-  if (bodyPartFilter !== '') {
+  if (equipmentFilter !== '' || includeArms || includeLegs || includeChest || includeShoulder || includeBack) {
     displayedData = displayedData.filter((data) => {
-      if (data.body_part === bodyPartFilter) {
+      if (data.equipment === equipmentFilter) {
+        return true;
+      }
+      if (includeArms && data.body_part === 'Arms') {
+        return true;
+      }
+      if (includeLegs && data.body_part === 'Legs') {
+        return true;
+      }
+      if (includeChest && data.body_part === 'Chest') {
+        return true;
+      }
+      if (includeShoulder && data.body_part === 'Shoulder') {
+        return true;
+      }
+      if (includeBack && data.body_part === 'Back') {
         return true;
       }
       return false;
     });
   }
 
-  const applyFilter = (bodyPartName) => {
-    setBodyPartFilter(bodyPartName);
+  const applyFilter = (equipmentFilter, includeArms, includeLegs, includeChest, includeShoulder, includeBack) => {
+    setEquipmentFilter(equipmentFilter);
+    setIncludeArms(includeArms);
+    setIncludeLegs(includeLegs);
+    setIncludeChest(includeChest);
+    setIncludeShoulder(includeShoulder);
+    setIncludeBack(includeBack);
   };
 
   return (
     <div className="container">
-      <Header/>
-      {/* <Search/> */}
-      {/* <Equipment/> */}
-      {/* <TopRow/> */}
-      {/* <BottomRow/> */}
-      <ExerciseSelectForm bodyPartOptions={uniqueExercise} applyFilterCallback={applyFilter} />
+      <Header />
+      {/* <Search /> */}
+      {/* <Equipment /> */}
+      {/* <TopRow /> */}
+      {/* <BottomRow /> */}
+      <p>Please select the type of equipment and body part to filter the approriate exercise for you.</p>
+      <ExerciseSelectForm
+        equipmentOptions={uniqueExercise}
+        includeArms={includeArms}
+        includeLegs={includeLegs}
+        includeChest={includeChest}
+        includeShoulder={includeShoulder}
+        includeBack={includeBack}
+        applyFilterCallback={applyFilter}
+      />
       <CardList data={displayedData} />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/ExerciseSelectForm.js
+++ src/components/ExerciseSelectForm.js
@@ -1,32 +1,84 @@
-import React, { useState } from 'react'; //import React Component
+import React, { useState } from 'react';
 
 export default function ExerciseSelectForm(props) {
+  const [selectedEquipment, setSelectedEquipment] = useState('');
+  const [includeArms, setIncludeArms] = useState(false);
+  const [includeLegs, setIncludeLegs] = useState(false);
+  const [includeChest, setIncludeChest] = useState(false);
+  const [includeShoulder, setIncludeShoulder] = useState(false);
+  const [includeBack, setIncludeBack] = useState(false);
 
-  //Your work goes here
-  const [selectedBodyPart, setSelectedBodyPart] = useState('');
+  const handleEquipmentChange = (event) => {
+    setSelectedEquipment(event.target.value);
+  }
 
-  const handleBodyPartChange = (event) => {
-    setSelectedBodyPart(event.target.value);
+  const handleArmsChange = (event) => {
+    setIncludeArms(event.target.checked);
+  }
+
+  const handleLegsChange = (event) => {
+    setIncludeLegs(event.target.checked);
+  }
+
+  const handleChestChange = (event) => {
+    setIncludeChest(event.target.checked);
+  }
+
+  const handleShoulderChange = (event) => {
+    setIncludeShoulder(event.target.checked);
+  }
+
+  const handleBackChange = (event) => {
+    setIncludeBack(event.target.checked);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // handle form submission with body part selected
-    props.applyFilterCallback(selectedBodyPart);
+    props.applyFilterCallback(selectedEquipment, includeArms, includeLegs, includeChest, includeShoulder, includeBack);
   }
 
-  const optionElems = props.bodyPartOptions.map((bodyPart) => {
-    return <option key={bodyPart} value={bodyPart}>{bodyPart}</option>
+  const optionElems = props.equipmentOptions.map((equipment) => {
+    return <option key={equipment} value={equipment}>{equipment}</option>
   })
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="row align-items-center mb-3">
         <div className="col-auto">
-          <select id="bodyPartSelect" className="form-select" value={selectedBodyPart} onChange={handleBodyPartChange}>
-            <option value="">Show all exercise</option>
+          <select id="equipmentSelect" className="form-select" value={selectedEquipment} onChange={handleEquipmentChange}>
+            <option value="">Show all equipments</option>
             {optionElems}
           </select>
+        </div>
+        <div className="col-auto">
+          <div className="form-check">
+            <input id="armsCheckbox" type="checkbox" className="form-check-input" checked={includeArms} onChange={handleArmsChange} />
+            <label htmlFor="armsCheckbox" className="form-check-label">Arms</label>
+          </div>
+        </div>
+        <div className="col-auto">
+          <div className="form-check">
+            <input id="legsCheckbox" type="checkbox" className="form-check-input" checked={includeLegs} onChange={handleLegsChange} />
+            <label htmlFor="legsCheckbox" className="form-check-label">Legs</label>
+          </div>
+        </div>
+        <div className="col-auto">
+          <div className="form-check">
+            <input id="chestCheckbox" type="checkbox" className="form-check-input" checked={includeChest} onChange={handleChestChange} />
+            <label htmlFor="chestCheckbox" className="form-check-label">Chest</label>
+          </div>
+        </div>
+        <div className="col-auto">
+          <div className="form-check">
+            <input id="shoulderCheckbox" type="checkbox" className="form-check-input" checked={includeShoulder} onChange={handleShoulderChange} />
+            <label htmlFor="shoulderCheckbox" className="form-check-label">Shoulder</label>
+          </div>
+        </div>
+        <div className="col-auto">
+          <div className="form-check">
+            <input id="backCheckbox" type="checkbox" className="form-check-input" checked={includeBack} onChange={handleBackChange} />
+            <label htmlFor="backCheckbox" className="form-check-label">Back</label>
+          </div>
         </div>
         <div className="col-auto">
           <button id="submitButton" type="submit" className="btn btn-warning">Apply Filter</button>
