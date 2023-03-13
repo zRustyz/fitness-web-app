e1b4a5b81ddbf708ecf993fdfad557148b4d2996
YonasM2 <yonasgm@uw.edu>
2023-03-13 16:33:27 -0700

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,10 @@ function About(props) {
         <h1 className="m-3">About Us</h1>
       </header>
       <main>
-        <p className='m-3'>This website is for fitness style that provides some recommended exercises for your personal goals.</p>
+        <p className='m-3'>Welcome to our fitness app! Our app is designed to help you get in shape by providing you with exercises based on specific body parts, 
+        along with instructions on how to perform them correctly. Our app includes a comprehensive list of exercises that are grouped by body part, including chest,
+        back, arms, legs, and core. Each exercise is accompanied by step-by-step instructions, along with pictures to guide you through proper form and technique.
+        This ensures that you perform each exercise safely and effectively, which is essential for achieving the best possible results.</p>
       </main>
       <Footer />
     </div>
